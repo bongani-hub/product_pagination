2d1f60cb4e90567c442a40394db782916aaaa921
bongani-hub <94044452+bongani-hub@users.noreply.github.com>
2022-12-14 20:38:59 +0200

    first commit

---
+++ src/components/Images.js
@@ -0,0 +1,54 @@
+
+import { useEffect,useState } from 'react'
+import ReactPaginate from 'react-paginate';
+
+export default function Images(props) {
+    const { data } = props;
+    const [currentItems,setCurrentItems] = useState([]);
+    const [itemOffset, setItemOffset] = useState(0);
+    const [pageCount,setPageCount] =useState(0)
+    const itemsPerPage = "1";
+
+    
+  useEffect(()=>{ 
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data.slice(itemOffset,endOffset));
+    setPageCount(Math.ceil(data.length / itemsPerPage));
+}, [itemOffset,itemsPerPage, data])
+
+  
+   
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % data.length;
+      setItemOffset(newOffset);
+    };
+  
+    return (
+      <>
+        <div className="images">
+            {currentItems.map(image => {
+                return(
+                <div className="image"> 
+                  <img src={image.url} alt={image.title}/>  
+                </div>
+                );
+            })}
+           
+        </div>
+         
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageLinkClassName='page-num'
+          nextLinkClassName='page-num'
+          activeLinkClassName='active'
+        />
+       </>
+    );
+}
